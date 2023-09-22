08272ee892ba15b64943db2921610877ca49815c
Домашний <@DESKTOP-2F3EUOR>
2023-09-22 15:12:53 +0300

    Добавьте файлы проекта.

---
+++ Pages/PageAddEdit.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Глазкова_Труфанова.Classes;
+
+namespace Глазкова_Труфанова.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddEdit.xaml
+    /// </summary>
+    public partial class PageAddEdit : Page
+    {
+        public PageAddEdit()
+        {
+            InitializeComponent();
+
+           
+            CmbCar.ItemsSource = Auto_repair_shopsEntities.GetContext().car.ToList();
+            CmbCar.SelectedValuePath = "id_car";
+            CmbCar.DisplayMemberPath = "car_number";
+
+            CmbDriver.ItemsSource = Auto_repair_shopsEntities.GetContext().Driver.ToList();
+            CmbDriver.SelectedValuePath = "id_driver";
+            CmbDriver.DisplayMemberPath = "full_name";
+
+            CmbMechanic.ItemsSource = Auto_repair_shopsEntities.GetContext().Mechanic.ToList();
+            CmbMechanic.SelectedValuePath = "id_mechanic";
+            CmbMechanic.DisplayMemberPath = "full_name";
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            repair rep = new repair()
+            {
+                id_car = int.Parse(CmbCar.SelectedValue.ToString()),
+                id_driver = int.Parse(CmbDriver.SelectedValue.ToString()),
+                id_mechanic = int.Parse(CmbMechanic.SelectedValue.ToString()),
+                price = int.Parse(TxtPrice.Text)
+            };
+            Auto_repair_shopsEntities.GetContext().repair.Add(rep);
+            Auto_repair_shopsEntities.GetContext().SaveChanges();
+
+            MessageBoxResult boxResult = MessageBox.Show("Данные добавлены. Добавить еще?",
+                "Сообщение", MessageBoxButton.YesNo);
+            if (boxResult == MessageBoxResult.Yes)
+            {
+                //int.Parse(CmbCar.Clear)();
+                //int.Parse(CmbDriver.Clear)();
+                //CmbCar.Focus();
+            }
+            else
+                Classes.ClassFrame.frmObj.Navigate(new Pages.PageRepair());
+        }
+    }
+}
---
+++ Pages/PageRepair.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Глазкова_Труфанова.Classes;
+
+namespace Глазкова_Труфанова.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageRepair.xaml
+    /// </summary>
+    public partial class PageRepair : Page
+    {
+        public PageRepair()
+        {
+            InitializeComponent();
+
+            dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().repair.ToList();
+            CmbPrice.ItemsSource = Auto_repair_shopsEntities.GetContext().repair.Select(x=>x.price).Distinct().ToList();
+            CmbFullName.ItemsSource = Auto_repair_shopsEntities.GetContext().Driver.ToList();
+            CmbFullName.SelectedValuePath = "id_driver";
+            CmbFullName.DisplayMemberPath = "full_name";
+        }
+
+        private void BtnResetFiltr_Click(object sender, RoutedEventArgs e)
+        {
+            dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().repair.ToList();
+        }
+
+        private void CmbPrice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            decimal price = (decimal)(CmbPrice.SelectedValue);
+            dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().repair.Where(x => x.price == price).ToList();
+        }
+
+        private void CmbFullName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int dr = int.Parse(CmbFullName.SelectedValue.ToString());
+            dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().repair.Where(x => x.id_driver == dr).ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ClassFrame.frmObj.Navigate(new PageAddEdit());
+        }
+
+        private void TxtSearchName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int cn = int.Parse(CmbFullName.SelectedValue.ToString());
+            dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().car.Where(x => x.id_car == cn).ToList();
+            //string search = TxtSearchName.Text;
+            //dtgRepair.ItemsSource = Auto_repair_shopsEntities.GetContext().car.Where(x => x.car_number.Contains(search)).ToList();
+            /*Where(x => x.car_number.search).ToList();*/
+        }
+    }
+}
